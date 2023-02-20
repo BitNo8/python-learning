b4374a94ea577c109a47d09c7247e4b3fab0d189
BitNo8 <125727897+BitNo8@users.noreply.github.com>
2023-02-20 17:01:06 -0500

    Add titles to 1-control-flow challenges

--- Assignments/6-optional-challenges/1-control-flow.py
+++ Assignments/6-optional-challenges/1-control-flow.py
@@ -1,4 +1,4 @@
-# 1. Test wether result of taking the power of one number to another number provides an answer which is greater than 5000
+# 1. Large Power: Test wether result of taking the power of one number to another number provides an answer which is greater than 5000
 
 # Write your large_power function here:
 def large_power(base, exponent):
@@ -20,7 +20,7 @@ print(large_power(2, 12))
 
 #-------------------------------------------------------------------------------
 
-# 2. Our function will accept a parameter called budget which describes our spending limit. The next four parameters describe what we are spending our money on. We need to sum all of our spendings and compare it to the budget. If we have gone over budget, we will return True. Otherwise we return False.
+# 2. Over Budget: Our function will accept a parameter called budget which describes our spending limit. The next four parameters describe what we are spending our money on. We need to sum all of our spendings and compare it to the budget. If we have gone over budget, we will return True. Otherwise we return False.
 
 # Write your over_budget function here:
 def over_budget(budget, food_bill, electricity_bill, internet_bill, rent):
@@ -42,7 +42,7 @@ print(over_budget(80, 20, 30, 10, 30))
 
 #-------------------------------------------------------------------------------
 
-# 3. In this challenge, we will determine if one number is twice as large as another number. To do this, we will compare the first number with two times the second number.
+# 3. Twice as Large: In this challenge, we will determine if one number is twice as large as another number. To do this, we will compare the first number with two times the second number.
 
 # Write your twice_as_large function here:
 def twice_as_large(num1, num2):
@@ -61,7 +61,7 @@ print(twice_as_large(11, 5))
 
 #-------------------------------------------------------------------------------
 
-# 4. To make things a bit more challenging, we are going to create a function that determines whether or not a number is divisible by ten. A number is divisible by ten if the remainder of the number divided by 10 is 0.
+# 4. Divisible by Ten: To make things a bit more challenging, we are going to create a function that determines whether or not a number is divisible by ten. A number is divisible by ten if the remainder of the number divided by 10 is 0.
 
 # Write your divisible_by_ten() function here:
 
@@ -81,7 +81,7 @@ print(divisible_by_ten(25))
 
 #-------------------------------------------------------------------------------
 
-# 5. Finally, we are going to check if the summation of two inputs does not equal ten. Our function will accept two inputs and add them together. If the two numbers added together are not equal to ten, then we will return True, otherwise, we will return False.
+# 5. Not Sum to Ten: Finally, we are going to check if the summation of two inputs does not equal ten. Our function will accept two inputs and add them together. If the two numbers added together are not equal to ten, then we will return True, otherwise, we will return False.
 
 # Write your not_sum_to_ten function here:
   
