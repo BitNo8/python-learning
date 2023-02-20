14c094d51e26b9fba9692b5ce35a584fbf074820
BitNo8 <125727897+BitNo8@users.noreply.github.com>
2023-02-20 14:36:17 -0500

    Add get_force function & clean up code

--- physics-test.py
+++ physics-test.py
@@ -1,25 +1,34 @@
-#Initialize train variables
+# Initialize physics-test variables
 train_mass = 22680
 train_accel = 10
 train_distance = 100
-
-#Initialize bomb variable
 bomb_mass = 1
 
-#Function: Convert Celcius to Farenheight
+
+# Convert Celcius to Farenheight
 def c_to_f(c_temp):
-    f_temp = (c_temp * 9/5) + 32
+    f_temp = (c_temp * 9 / 5) + 32
     return f_temp
 
-#Testing c_to_f
-#c0_in_farenheit = c_to_f(0)
-#print("0 degrees is",round(c0_in_farenheit, 1), "degrees Farenheight.")
+# Test
+c0_in_farenheit = c_to_f(0)
+print("\n0 degrees is", round(c0_in_farenheit, 1), "degrees Farenheight.")
 
-#Function: Convert Fahrenheit to Celcius
+
+# Convert Fahrenheit to Celcius
 def f_to_c(f_temp):
-    c_temp = (f_temp - 32) * (5/9)
+    c_temp = (f_temp - 32) * (5 / 9)
     return c_temp
 
-#Testing f_to_c
-#f100_in_celcius = f_to_c(100)
-#print("100 degrees is",round(f100_in_celcius, 1), "degrees Celcius.")
+# Test
+f100_in_celcius = f_to_c(100)
+print("\n100 degrees is", round(f100_in_celcius, 1), "degrees Celcius.")
+
+
+# Calculate Force
+def get_force(mass, acceleration):
+    return mass * acceleration
+
+# Test
+train_force = get_force(train_mass, train_accel)
+print("\nThe GE train supplies", train_force, "Newtons of force.")
