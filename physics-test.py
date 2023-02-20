6ee75c4ee0b54f54f1ea3e53866d768b0d1b1413
BitNo8 <125727897+BitNo8@users.noreply.github.com>
2023-02-20 14:11:19 -0500

    Consolidate temp functions

--- physics-test.py
+++ physics-test.py
@@ -14,3 +14,12 @@ def c_to_f(c_temp):
 #Testing c_to_f
 #c0_in_farenheit = c_to_f(0)
 #print("0 degrees is",round(c0_in_farenheit, 1), "degrees Farenheight.")
+
+#Function: Convert Fahrenheit to Celcius
+def f_to_c(f_temp):
+    c_temp = (f_temp - 32) * (5/9)
+    return c_temp
+
+#Testing f_to_c
+#f100_in_celcius = f_to_c(100)
+#print("100 degrees is",round(f100_in_celcius, 1), "degrees Celcius.")
